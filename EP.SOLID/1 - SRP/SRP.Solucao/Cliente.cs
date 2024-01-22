b917b3b9b5340fc8709683b839e85344b946a026
Edson de Araujo <edsonaraujo1@outlook.com>
2024-01-22 14:58:07 -0300

    segundo commit

---
+++ EP.SOLID/1 - SRP/SRP.Solucao/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EP.SOLID.SRP.Solucao
+{
+    // modelo cliente
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public bool IsValid()
+        {
+            return EmailServices.IsValid(Email) && CPFServices.IsValid(CPF);
+        }
+    }
+}
